595916867dc6a9a9e7d8c5d0df77e213ff2d9eed
Tomoki Yamashita <tomorrowkey@gmail.com>
2016-11-21 17:34:51 +0900

    Add options for setting target repo and issues

--- lib/backup_github_issues/executor.rb
+++ lib/backup_github_issues/executor.rb
@@ -12,10 +12,14 @@ class Executor
   def execute
     client = Github::Client.new(config.api_endpoint, config.access_token)
     (repos = client.org_repos(config.organization)).each do |repo|
+      next unless target_repo?(repo)
+
       puts "# #{repo.full_name}"
 
       (issues = repo.issues).each do |issue|
         next if renderer.skip?(issue)
+        next unless target_issue?(issue)
+
         puts "- #{issue.number} #{issue.title}"
 
         renderer.render_issue_page(issue)
@@ -27,5 +31,13 @@ class Executor
     end
 
     renderer.render_index_page(config.organization, repos)
+  end
+
+  def target_repo?(repo)
+    ENV['REPO'] == repo.name
+  end
+
+  def target_issue?(issue)
+    ENV['ISSUE_NUM'] == issue.number.to_s
   end
 end
